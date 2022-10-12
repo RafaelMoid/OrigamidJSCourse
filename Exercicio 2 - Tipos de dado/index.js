9d31cbc5135697bde0026cb231bb1c43d47ca131
Rafael <rafaelvrcr@gmail.com>
2022-10-11 22:13:28 -0300

    Template Strings

--- Exercicio 2 - Tipos de dado/index.js
+++ Exercicio 2 - Tipos de dado/index.js
@@ -20,3 +20,14 @@ console.log(typeof nomeCompleto);
 // Como fazer com que "aspas duplas" ou 'aspas simples'
 // sejam validas em uma string
 console.log('Basta fazer com está no \"VsCode\" na linha 22');
+
+// Qual o beneficio de uma Template Strings
+
+// String normal
+var cardsEDH = 100;
+var fraseDeck = 'um deck de EDH contém ' + cardsEDH + ' cartas.';
+console.log(fraseDeck);
+
+// Template String
+var fraseDeck2 = `Um deck de EDH contém ${cardsEDH} cartas.`;
+console.log(fraseDeck2);
