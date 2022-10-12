4671d75f4874fe9340c31b7e34d470660f79c5cc
Rafael <rafaelvrcr@gmail.com>
2022-10-11 21:39:26 -0300

    I think ive made it

---
+++ exercicio1.js
@@ -0,0 +1,18 @@
+// Declarar uma variável com o seu nome
+var nome = 'Rafael';
+
+// Declarar uma variável com a sua idade
+var idade = '32';
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Pizza',
+
+// Declarar 5 variáveis diferentes sem valores
+var commander,
+    Deck,
+    Anime,
+    Filho,
+    Esposa;
