4c2811273b3c9643f9ff4a121e14c35e6d92362b
Rafael <rafaelvrcr@gmail.com>
2022-10-11 21:49:41 -0300

    Declaration correction

--- exercicio1.js
+++ exercicio1.js
@@ -2,17 +2,19 @@
 var nome = 'Rafael';
 
 // Declarar uma variável com a sua idade
-var idade = '32';
+var idade = 32;
 
 // Declarar uma variável com a sua comida favorita e não atribuir valor
 var comidaFavorita;
 
 // Atribuir valor a sua comida favorita
-comidaFavorita = 'Pizza',
+comidaFavorita = 'Pizza';
 
 // Declarar 5 variáveis diferentes sem valores
 var commander,
-    Deck,
-    Anime,
-    Filho,
-    Esposa;
+    deck,
+    anime,
+    filho,
+    esposa;
+
+    console.log(nome, idade, comidaFavorita, commander, deck, anime, filho, esposa);
