bcb1ff7d39dc897988a12df0a1812491a59766d4
Rafael <rafaelvrcr@gmail.com>
2022-11-04 11:20:04 -0300

    loops

---
+++ Arrays e Loops/index.js
@@ -0,0 +1,22 @@
+var videoGames = ['Switch', 'PS4', 'Xbox'];
+
+//Loop de videoGames
+videoGames.forEach(element => {
+  console.log(element);
+});
+
+//Removendo o ultimo item da array
+var ultimoItem = videoGames.pop();
+
+//Loop FOR
+// é composto de 3 partes: INICIO, CONDIÇÃO e INCREMENTO
+for (var numero = 0; numero <= 10; numero++) {
+  console.log(numero);
+}
+
+//Loop While
+var i = 0;
+while (i <= 100) {
+  console.log(i);
+  i = i + 9;
+}
